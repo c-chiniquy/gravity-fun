0c93d129dc339ffe2708d1a473ba16b050d2e016
Christoffer Chiniquy <cc.dev.24@gmail.com>
2025-02-27 16:22:59 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
-REM This batch file builds the project for Visual Studio 2019.
+REM This batch file builds for Visual Studio 2019.
 cmake -B build -G "Visual Studio 16 2019" -A x64
+cmake --build build
 start build/gravity-fun.sln
